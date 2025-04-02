4990a41d1ec3733dffaa29d61094354a92dcf25e
Josue_Jimenez_F <darkbullet428@gmail.com>
2025-04-02 10:04:07 -0400

    Casi me olvido

---
+++ ejer02.py
@@ -0,0 +1,27 @@
+def suma_iterativa(n):
+    suma = 0
+    for i in range (1, n + 1):
+        suma +=i**2
+    return suma
+
+def suma_recursiva(n):
+    if n == 0:
+        return 0
+    return n**2 + suma_recursiva((n -1))
+
+def suma_formula(n):
+    return (n*(n+1)*(2*n+1))/6
+
+x = 100
+
+assert suma_iterativa(x) == suma_recursiva(x) == suma_formula(x), "¡Las funciones no coinciden!"
+
+print(" Todas las funciones dan el mismo resultado.")
+
+import timeit
+
+n = 10
+
+print("tiempo iterativa: ", timeit.timeit(lambda: suma_iterativa(n)), "segundos")
+print("tiempo recursivo: ", timeit.timeit(lambda: suma_recursiva(n)), "segundos")
+print("tiempo formula: ", timeit.timeit(lambda: suma_formula(n)), "segundos")
